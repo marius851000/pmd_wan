95e9f5be40846b130369ff79556e2a86c809e906
marius david <mariusdavid@laposte.net>
2021-03-11 20:43:27 +0100

    use thiserror

--- src/wan.rs
+++ src/wan.rs
@@ -2,75 +2,57 @@
 #![allow(clippy::len_without_is_empty)]
 #![allow(clippy::cognitive_complexity)]
 
+//TODO: use byteorder
+
 extern crate image;
-use std::error::Error;
-use std::fmt;
 use std::io;
 use std::io::{Read, Seek, SeekFrom, Write};
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum WanError {
-    IOError(io::Error),
+    #[error("an input/output error happened")]
+    IOError(#[from] io::Error),
+    #[error("an image id reference the previous one, but it is the first image")]
     ImageIDPointBackButFirstImage,
+    #[error("a metaframe is inferior to -1, but that is not valid (it is {0})")]
     MetaFrameLessThanLessOne(i16),
+    #[error("While creating a meta frame store: the check for the offset of the pointer of the animation group are not valid!")]
     InvalidOffset,
+    #[error("the resolution for an image wasn't found")]
     InvalidResolution,
+    #[error("pointer to image parts are not coherent")]
     IncoherentPointerToImagePart,
+    #[error("impossible to find a definied z_index (the image is probably empty)")]
     NoZIndex,
+    #[error("an invalid alpha level was found in the picture")]
     ImpossibleAlphaLevel,
+    #[error("an image data pointer is null")]
     NullImagePointer,
+    #[error("the image does not have a resolution")]
     ImageWithoutResolution,
+    #[error("the palette data doesn't end with 0s")]
     PaletteDontEndWithZero,
+    #[error("a reference to a color in a palette would overflow")]
     PaletteOOB,
+    #[error("can't find a specific color in the palette")]
     CantFindColorInPalette,
+    #[error("the sir0 header in invalid, expected SIR0, found {0:?}")]
     InvalidSir0([u8; 4]),
+    #[error("the end of the sir0 header should be four 0, found {0:?}")]
     InvalidEndOfSir0Header([u8; 4]),
+    #[error("the type of sprite is unknown (found the sprite type id {0}, but this program only known sprite for [0, 1, 3])")]
     TypeOfSpriteUnknown(u16),
+    #[error("the 2 byte that indicate the number of color is invalid (found {0}, expected 0 or 1")]
     InvalidColorNumber(u16),
+    #[error("the value of a substraction is less than 0: {0}-{1} ({2}-{3})")]
     OverflowSubstraction(u64, u64, &'static str, &'static str),
+    #[error("the value of an addition is more than the maximum possible value: {0}+{1} ({2}+{3})")]
     OverflowAddition(u64, u64, &'static str, &'static str),
-    InlineError(&'static str),
-}
-
-impl From<io::Error> for WanError {
-    fn from(err: io::Error) -> Self {
-        Self::IOError(err)
-    }
-}
-
-impl Error for WanError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        match self {
-            Self::IOError(err) => Some(err),
-            _ => None,
-        }
-    }
-}
-impl fmt::Display for WanError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::IOError(_) => write!(f, "an io error happened"),
-            Self::ImageIDPointBackButFirstImage => write!(f, "an image id point to the same than the previous one, but it is the first image"),
-            Self::MetaFrameLessThanLessOne(id) => write!(f, "a metaframe is inferior to -1, but that is not valid (it actually is {})", id),
-            Self::InvalidOffset => write!(f, "in the creation of a meta frame store: the check for the offset of the pointer of the animation group are not valid!"),
-            Self::InvalidResolution => write!(f, "the resolution was not found!!!"),
-            Self::IncoherentPointerToImagePart => write!(f, "pointer to image parts are not coherent."),
-            Self::NoZIndex => write!(f, "impossible to find a definied z_index (the image is probably empty)!!! aborting"),
-            Self::ImpossibleAlphaLevel => write!(f, "an impossible alpha level was found in the picture !"),
-            Self::NullImagePointer => write!(f, "an image data pointer is null !!!"),
-            Self::ImageWithoutResolution => write!(f, "the image does not have a resolution"),
-            Self::PaletteDontEndWithZero => write!(f, "the palette data doesn't end with 0s !!!"),
-            Self::PaletteOOB => write!(f, "impossible to get a color in the palette, as this will do an OOB."),
-            Self::CantFindColorInPalette => write!(f, "impossible to find the specified color in the palette!"),
-            Self::InvalidSir0(value) => write!(f, "the sir0 header in invalid, expected SIR0, found {:?}", value),
-            Self::InvalidEndOfSir0Header(value) => write!(f, "the end of the sir0 header should be four 0, found {:?}", value),
-            Self::TypeOfSpriteUnknown(value) => write!(f, "the type of sprite is unknown (found the sprite type id {}, but this program only known sprite for [0, 1, 3])", value),
-            Self::InvalidColorNumber(value) => write!(f, "the 2 byte that indicate the number of color is invalid (found {}, expected 0 or 1)", value),
-            Self::OverflowSubstraction(value1, value2, var_name1, var_name2) => write!(f, "the value of a substraction is less than 0: {}-{} ({}-{})", value1, value2, var_name1, var_name2),
-            Self::OverflowAddition(value1, value2, var_name1, var_name2) => write!(f, "the value of an addition is more than the maximum possible value: {}+{} ({}+{})", value1, value2, var_name1, var_name2),
-            Self::InlineError(message) => write!(f, "the file format is invalid, but without a lenghtly explanation: \"{}\".", message),
-        }
-    }
+    #[error("the resolution of a sprite is too small accept all it's pixel")]
+    SpriteTooSmall,
+    #[error("an image doesn't have a constant depth index")]
+    NonConstantIndexInImage,
 }
 
 fn get_bit_u16(byte: u16, id: u16) -> Option<bool> {
