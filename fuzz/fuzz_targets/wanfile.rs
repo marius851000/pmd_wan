e4b48e0c5056cfb1b54b27a416a56286154bc351
marius david <mariusdavid@laposte.net>
2021-03-15 00:09:05 +0100

    corrected the fuzzer for read only

--- fuzz/fuzz_targets/wanfile.rs
+++ fuzz/fuzz_targets/wanfile.rs
@@ -2,15 +2,15 @@
 #[macro_use] extern crate libfuzzer_sys;
 extern crate pmd_wan;
 use std::io::Cursor;
+
 fuzz_target!(|data: &[u8]| {
     let input = Cursor::new(data);
     let _result = pmd_wan::WanImage::new(input);
     /*match result {
-        Ok(data) => for image in data.image_store.images {
-            if image.img.width() > 3 && image.img.height() > 3 {
-                image.img.save("./tmp.png").unwrap();
-            };
-        },
         Err(_) => (),
+        Ok(valid) => {
+            let mut output = Cursor::new(Vec::new());
+            pmd_wan::WanImage::create_wan(&valid, &mut output).unwrap(); //TODO: change the API
+        }
     }*/
 });
