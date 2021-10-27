d5b8d4a34c10ee183d3c8efe9ef7690d98e3418c
marius david <m.david@bimedia.org>
2021-10-27 17:15:32 +0200

    added benchmark

---
+++ benches/parse.rs
@@ -0,0 +1,18 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use pmd_wan::WanImage;
+use std::io::Cursor;
+use std::fs::File;
+use std::io::Read;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut wan_file = File::open("bulbasaurEU.wan").unwrap();
+    let mut wan_data = Vec::new();
+    wan_file.read_to_end(&mut wan_data).unwrap();
+
+    c.bench_function("load image", |b| b.iter(|| WanImage::new(
+        Cursor::new(&wan_data)
+    ).unwrap()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
