e6e8510868636bd3ffc89327ab9b3be0a4ff8a26
marius david <mariusdavid@laposte.net>
2021-11-25 13:33:36 +0100

    rewrite of test_read_write for simplicity

--- test_read_write/src/main.rs
+++ test_read_write/src/main.rs
@@ -1,10 +1,9 @@
 use clap::Parser;
-use image::ImageFormat;
-use pmd_wan::WanImage;
+use pmd_wan::{WanImage};
 use std::{
     fs::{read_dir, File},
-    io::{Cursor, Read, Seek, SeekFrom, Write},
-    path::{Path, PathBuf},
+    io::{Cursor, Read, Seek, SeekFrom},
+    path::{PathBuf},
 };
 
 #[derive(Parser, Debug)]
@@ -12,33 +11,19 @@ struct Opts {
     decompressed_pmd: PathBuf,
 }
 
-fn test_read_reencode(path: &Path) {
-    println!("trying {:?}", path);
-    let mut f = File::open(&path).unwrap();
-    let mut buffer = Vec::new();
-    f.read_to_end(&mut buffer).unwrap();
-    let buffer_in = buffer.clone();
-    let input = Cursor::new(buffer);
-    let out_buffer = Vec::new();
-    let mut output = Cursor::new(out_buffer);
-    let wan_image = WanImage::new(input).unwrap();
-    wan_image.create_wan(&mut output).unwrap();
-    output.seek(SeekFrom::Start(0)).unwrap();
-    let out_buffer = output.into_inner();
-    let out_buffer_clone = out_buffer.clone();
-    let wan_image_redecoded = WanImage::new(Cursor::new(out_buffer_clone)).unwrap();
-    if wan_image != wan_image_redecoded {
-        let mut i = File::create("./in.bin").unwrap();
-        let mut o = File::create("./out.bin").unwrap();
-        i.write_all(&buffer_in).unwrap();
-        o.write_all(&out_buffer).unwrap();
-        if wan_image.image_store.images[0].pixels
-            != wan_image_redecoded.image_store.images[0].pixels
-        {
-            panic!("good");
-        }
-        panic!("the input and the output of the file at {:?} are different. Content written in \"in.bin\" and \"out.bin\"", path);
-    };
+fn test_read_reencode<F: Read + Seek>(content: &mut F, source: &str) {
+    //read
+    let original_wan = WanImage::new(content).unwrap();
+    //write
+    let rewrite_buffer: Vec<u8> = Vec::new();
+    let mut rewrite_cursor = Cursor::new(rewrite_buffer);
+    original_wan.create_wan(&mut rewrite_cursor).unwrap();
+    //re-read
+    rewrite_cursor.seek(SeekFrom::Start(0)).unwrap();
+    let reread_wan = WanImage::new(rewrite_cursor).unwrap();
+    if original_wan != reread_wan {
+        panic!("failed to correctly read, write and re-read the written file for {}", source);
+    }
 }
 
 fn main() {
@@ -50,8 +35,10 @@ fn main() {
     for entry in read_dir(opts.decompressed_pmd.join("GROUND")).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
+        let mut f = File::open(&path).unwrap();
         if path.extension().unwrap() == "wan" {
-            test_read_reencode(&path);
+            test_read_reencode(&mut f, &path.to_string_lossy());
         }
     }
+    //test_read_reencode(&PathBuf::from("/home/marius/pmdeu/GROUND/d01p11b2.wan"));
 }
