7e2292bb32f36ac81b9ab3a22902ab5e68b5ecda
marius david <mariusdavid@laposte.net>
2021-11-26 21:57:29 +0100

    enhanced the fuzzing to try to prevent crashes

--- fuzz/fuzz_targets/wanfile.rs
+++ fuzz/fuzz_targets/wanfile.rs
@@ -2,17 +2,22 @@
 #[macro_use] extern crate libfuzzer_sys;
 extern crate pmd_wan;
 use std::io::Cursor;
+use std::io::{Seek, SeekFrom};
 
 fuzz_target!(|data: &[u8]| {
-    //TODO: env_logger::init();
     let input = Cursor::new(data);
-    let _result = pmd_wan::WanImage::new(input);
-    match result {
+    let decoded = pmd_wan::WanImage::decode_wan(input);
+    match decoded {
         Err(_) => (),
-        Ok(valid) => {
-            let mut output = Cursor::new(Vec::new());
-            pmd_wan::WanImage::create_wan(&valid, &mut output).unwrap(); //TODO: change the API
-            //TODO: check the equality
+        Ok(mut valid) => {
+            valid.anim_store.copied_on_previous = None;
+            let mut reread_file = Cursor::new(Vec::new());
+            valid.create_wan(&mut reread_file).unwrap();
+            reread_file.seek(SeekFrom::Start(0)).unwrap();
+            let mut reread_wan = pmd_wan::WanImage::decode_wan(reread_file).unwrap();
+            reread_wan.anim_store.copied_on_previous = None;
+            //TODO: ensure this hold true every time
+            //assert_eq!(reread_wan, valid);
         }
     }
 });
