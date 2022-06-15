dd4385ef61dbe1ee1015a3fe31daff4e36abae16
marius david <m.david@bimedia.org>
2022-06-15 13:10:35 +0200

    fixed benchmark

--- pmd_wan/benches/parse.rs
+++ pmd_wan/benches/parse.rs
@@ -5,11 +5,11 @@ use std::fs::File;
 use std::io::Read;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut wan_file = File::open("bulbasaurEU.wan").unwrap();
+    let mut wan_file = File::open("/home/marius/pmd_wan/bulbasaurEU.wan").unwrap();
     let mut wan_data = Vec::new();
     wan_file.read_to_end(&mut wan_data).unwrap();
 
-    c.bench_function("load image", |b| b.iter(|| WanImage::new(
+    c.bench_function("load image", |b| b.iter(|| WanImage::decode_wan(
         Cursor::new(&wan_data)
     ).unwrap()));
 }
