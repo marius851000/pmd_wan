61270c747561b8836802ecb956ef44f6e68eddb8
marius david <marius@mariusdavid.fr>
2022-08-24 19:45:18 +0200

    add a new benchmark for the fragment finder

---
+++ pmd_wan/benches/find_fragment.rs
@@ -0,0 +1,51 @@
+use std::{collections::HashMap, convert::TryInto, fs::File, io::BufReader};
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use image::GenericImageView;
+use pmd_wan::{find_fragments_in_images, GeneralResolution};
+
+const TILE_WIDTH: u32 = 82;
+const TILE_HEIGTH: u32 = 80;
+const TILE_NB_X: u32 = 15;
+const TILE_NB_Y: u32 = 15;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut img_file = BufReader::new(
+        File::open("/home/marius/Sync/programming_project/pmd_wan/reshiram.png").unwrap(),
+    );
+    let img_generic = image::load(&mut img_file, image::ImageFormat::Png).unwrap();
+    let img = img_generic.as_rgba8().unwrap();
+
+    let mut palette_map: HashMap<_, u8> = HashMap::new();
+    let mut tiles = Vec::new();
+    for tile_x in 0..TILE_NB_X {
+        for tile_y in 0..TILE_NB_Y {
+            let tile_start_x = tile_x * TILE_WIDTH;
+            let tile_start_y = tile_y * TILE_HEIGTH;
+            let tile = img.view(tile_start_x, tile_start_y, TILE_WIDTH, TILE_HEIGTH);
+            let mut tile_paletted = Vec::with_capacity(TILE_WIDTH as usize * TILE_HEIGTH as usize);
+            for (_, _, color) in tile.pixels() {
+                let id = *palette_map
+                    .entry(color)
+                    .or_insert_with(|| (tile_paletted.len() + 1).try_into().unwrap());
+                tile_paletted.push(id);
+            }
+            tiles.push(tile_paletted);
+        }
+    }
+
+    let mut find_fragments_entry = Vec::new();
+    for t in &tiles {
+        find_fragments_entry.push((
+            t.as_slice(),
+            GeneralResolution::new(TILE_WIDTH, TILE_HEIGTH),
+        ));
+    }
+
+    c.bench_function("find fragments", |b| {
+        b.iter(|| find_fragments_in_images(&find_fragments_entry).unwrap());
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
 [![forthebadge](https://forthebadge.com/images/badges/uses-git.svg)](https://forthebadge.com)
 
+Licensed under the creative–common 0 license (TODO: just put a LICENSE file)
+
 # pmd_wan
+Note that the information written bellow is outdated, and new function has been added. None has been removed, thought. (Still have to update it).
+
 read wan sprites, used in pokemon mystery dungeon: Explorers and Rescue Team (only tested with Explorers of Sky)
 
 reading : production ready. Should never panic, and has been fuzzed
@@ -12,3 +16,8 @@ status of writing sprites : ugly, may work but may panic or produce incorrect re
 - probably all ground sprite should be too
 
 creating an all new static sprite : Should work with no issue.
+
+## Executing benchs
+The benchs use real image not under the license of this repo, that you need to provide yourself.
+  * parse use the bulbasaur.wan in the m_ground.bin file. Can be exported with SkyTemple or another .bin EOS extractor.
+  * find_fragment use the White Kyurem sprite by FunnyKecleonMeme. It can be downloaded here : (TODO: actually put the download link)
